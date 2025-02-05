6bb0c64906ca4ebc0bc5cc6f0ea6c01af715dcf2
Antonio Martínez García <antonio.martinez-garcia@capgemini.com>
2025-02-05 12:16:03 +0100

    Pequeña reorganización de código

--- src/main/java/com/example/ejercicio_java/exceptions/libro/LibroException.java
+++ src/main/java/com/example/ejercicio_java/exceptions/libro/LibroException.java
@@ -1,6 +1,9 @@
 package com.example.ejercicio_java.exceptions.libro;
 
+
 public class LibroException extends RuntimeException {
+    public static final int NO_ENCONTRADO = 500;
+    public static final int ISBN_DUPLICADO = 501;
 
     public LibroException(int code, String mensaje)
     {
